fbee1df1e7b405b2da1cca4ed61c2cf30568fc02
Alexis <franklin-al@hotmail.com>
2019-03-21 20:26:36 -0500

    Iniciando proyecto

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+    <h1>Hola {{ msg }}</h1> <!-- con las llaves se indica que ahi va condigo javascript. Solo puede haber un elemento padre -->
+    <input type="text" v-model="msg"> <!-- Lo enlazado de conecta -->
+    <button @click="guardar">Guardar</button>
+    <table>
+        <tr>
+            <th>Nombre</th>
+        </tr>
+        <tr v-for="(mun,index) in municipios" :key="index"><!-- seria como mun dentro de municipios, index son las posiciones del for -->
+            <td>{{ mun }} </td>
+            </tr>  
+    </table>
+</div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            msg: "Alexis López",
+            municipios: ['Mocoa', 'Villagarzon', 'Pto Asis']
+
+        }
+    },
+    methods: {
+        guardar(){
+           // alert("guardando...")
+            this.municipios.push(this.msg)  /* this hace referencia a la instancia */
+            this.msg = ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
